c0d2c170e8c22f40092cc151d82d0d0ddad89632
kkucz256 <kkucz256@gmail.com>
2024-04-21 14:57:40 +0200

    Adding car to database

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ from flask import Flask, jsonify, request
 import psycopg2
 
 app = Flask(__name__)
-password = input("Enter password: ")
 db_params = {
     'dbname': 'defaultdb',
     'user': 'avnadmin',
-    'password': password,
+    'password': 'AVNS_QxzPpkrNJXOolRyltc4',
     'host': 'car-rental-car-rreennttaall.e.aivencloud.com',
     'port': '22365'
 }
@@ -19,43 +18,31 @@ def connect_to_db():
 
 @app.route('/')
 def index():
-    return 'Witaj w API naszej wypożyczalni samochodów!'
-
-
-@app.route('/cars', methods=['GET'])
-def get_cars():
-    conn = connect_to_db()
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM cars")
-    rows = cur.fetchall()
-    cur.close()
-    conn.close()
-    return jsonify(rows)
+    return 'Witaj w API wypożyczalni!'
 
 
 @app.route('/cars', methods=['POST'])
 def add_car():
     conn = connect_to_db()
     cur = conn.cursor()
-
     data = request.get_json()
-    brand = data.get('brand')
-    color = data.get('color')
 
-    cur.execute(f"SELECT * FROM cars WHERE brand LIKE '{brand}' AND color LIKE '{color}'")
-    existing_car = cur.fetchone()
-
-    if existing_car:
-        return jsonify({'message': f'Samochód marki {brand} o kolorze {color} już istnieje.'}), 400
-
-    cur.execute(f"INSERT INTO cars (brand, color) VALUES ('{brand}', '{color}')")
+    cur.execute(f"""INSERT INTO car (brand_id, status, price_per_day, year_of_production,
+                horsepower, engine_type, body, color_id, max_velocity, gearbox, seats_no,
+                deposit, last_rental_beginning, last_rental_end, place_id, photo) VALUES ('{data['brand_id']}',
+                 '{data['status']}', '{data['price_per_day']}', '{data['year_of_production']}', '{data['horsepower']}',
+                  '{data['engine_type']}', '{data['body']}', '{data['color_id']}', '{data['max_velocity']}',
+                   '{data['gearbox']}', '{data['seats_no']}', '{data['deposit']}', '{data['last_rental_beginning']}',
+                    '{data['last_rental_end']}', '{data['place_id']}', '{data['photo']}')""")
     conn.commit()
+    brand_id = data['brand_id']
+    cur.execute(f"SELECT brand FROM brand WHERE brand_id = {brand_id}")
+    brand_name = cur.fetchone()[0] if cur.rowcount > 0 else None
 
     cur.close()
     conn.close()
+    return f'Car {brand_name} added successfully!'
 
-    return jsonify({'message': f'Samochód {brand} o kolorze {color} dodany pomyślnie!'}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
-
--- main.py
+++ main.py
@@ -1,23 +1,79 @@
 import requests
+import json
+from datetime import datetime
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+last_rental_beginning = datetime(2024, 4, 19)
+last_rental_end = datetime(2024, 4, 21)
+formatted_last_rental_end = last_rental_end.strftime('%Y-%m-%d %H:%M:%S')
+formatted_last_rental_beginning = last_rental_beginning.strftime('%Y-%m-%d %H:%M:%S')
 
 
-brand = input("Podaj markę samochodu: ")
-color = input("Podaj kolor samochodu: ")
+class CarReservationApp(App):
+    def build(self):
+        layout = BoxLayout(orientation='vertical', padding=20, spacing=10, size_hint=(None, None), width=400,
+                           height=400)
+
+        titles = ['Price per day:', 'Year:', 'Horsepower:', 'Max velocity:', 'Seats no:']
+
+        self.text_inputs = []
+        for title in titles:
+            title_label = Label(text=title, size_hint_x=None, width=100, halign='right', pos_hint={'center_x': 0.5})
+            layout.add_widget(title_label)
+
+            text_input = TextInput(multiline=False, size_hint=(None, None), width=200, height=30,
+                                   pos_hint={'center_x': 0.5})
+            layout.add_widget(text_input)
+            self.text_inputs.append(text_input)
+
+        add_button = Button(text='Add', size_hint=(None, None), width=200, height=30, pos_hint={'center_x': 0.5})
+        layout.add_widget(add_button)
+
+        add_button.bind(on_press=self.extract_data)
+
+        layout.pos_hint = {'center_x': 0.5, 'center_y': 0.5}
+
+        return layout
+
+    def extract_data(self, instance):
+        price = self.text_inputs[0].text
+        year = self.text_inputs[1].text
+        hpw = self.text_inputs[2].text
+        velocity = self.text_inputs[3].text
+        seats = self.text_inputs[4].text
+
+        car_data = {
+            'brand_id': 1,
+            'status': 'free',
+            'price_per_day': price,
+            'year_of_production': year,
+            'horsepower': hpw,
+            'engine_type': 'diesel',
+            'body': 'wagon',
+            'color_id': 1,
+            'max_velocity': velocity,
+            'gearbox': 'manual',
+            'seats_no': seats,
+            'deposit': 1000,
+            'last_rental_beginning': formatted_last_rental_beginning,
+            'last_rental_end': formatted_last_rental_end,
+            'place_id': 1,
+            'photo': "https://i.imgur.com"
+        }
+
+        url = 'http://127.0.0.1:5000/cars'
+        response = requests.post(url, json=car_data)
+        if response.status_code == 200:
+            print(response.content.decode('utf-8'))
+        elif response.status_code == 400:
+            print("Bad request! Car data is invalid.")
+        else:
+            print(f"An error occurred! Status code: {response.status_code}")
 
 
-car_data = {
-    'brand': brand,
-    'color': color
-}
-
-
-url = 'http://127.0.0.1:5000/cars'
-
-response = requests.post(url, json=car_data)
-
-if response.status_code == 201:
-    print(response.json()['message'])
-elif response.status_code == 400:
-    print(response.json()['message'])
-else:
-    print(response.json()['message'])
+if __name__ == '__main__':
+    CarReservationApp().run()
