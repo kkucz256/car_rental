5f5142158c02190de0baa041267ead786b4e677d
buyablemos <263514@student.pwr.edu.pl>
2024-05-05 14:38:34 +0200

    added login and hashing

--- Login_Screen.py
+++ Login_Screen.py
@@ -1,46 +1,66 @@
+import requests
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.textinput import TextInput
 from Manager import ManagerScreen
+import hashlib
 
+
+def sha256_hash(data):
+    hasher = hashlib.sha256()
+    hasher.update(data.encode() if isinstance(data, str) else data)
+    return hasher.hexdigest()
 
 class MainScreen(Screen):
     def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
 
         label = Label(text='Welcome', size_hint=(None, None), size=(200, 50))
-        login_button = Button(text='Log-in', size_hint=(None, None), size=(200, 50))
+        login_button = Button(text='Log-in as user', size_hint=(None, None), size=(200, 50))
+        login_staff_button = Button(text='Log-in as staff', size_hint=(None, None), size=(200, 50))
         register_button = Button(text='Register', size_hint=(None, None), size=(200, 50))
 
         layout = BoxLayout(orientation='vertical', spacing=10, size_hint=(None, None), size=(200, 200),
                            pos_hint={'center_x': 0.5, 'center_y': 0.5})
         layout.add_widget(label)
         layout.add_widget(login_button)
+        layout.add_widget(login_staff_button)
         layout.add_widget(register_button)
 
         self.add_widget(layout)
 
         login_button.bind(on_press=self.switch_to_login_screen)
+        login_staff_button.bind(on_press=self.switch_to_login_screen_staff)
         register_button.bind(on_press=self.switch_to_register_screen)
 
     def switch_to_login_screen(self, instance):
         self.manager.current = 'login'
+
+    def switch_to_login_screen_staff(self, instance):
+        self.manager.current = 'staff'
 
     def switch_to_register_screen(self, instance):
         self.manager.current = 'register'
 
 
 class LoginScreen(Screen):
-    def __init__(self, **kwargs):
+    def __init__(self,info, **kwargs):
         super(LoginScreen, self).__init__(**kwargs)
         self.orientation = 'vertical'
         info_label = Label(text='', size_hint=(None, None), size=(200, 50))
+
+        if info == 'staff':
+            info2_label = Label(text='You are trying to login as staff', size_hint=(None, None), size=(200, 50))
+        else:
+            info2_label = Label(text='You are trying to login as user', size_hint=(None, None), size=(200, 50))
+
         username = Label(text='E-mail:', size_hint=(None, None), size=(200, 50))
-        username_text = TextInput(multiline=False, size_hint=(None, None), width=200, height=30)
+
+        self.username_text = TextInput(multiline=False, size_hint=(None, None), width=200, height=30)
         password = Label(text='Password:', size_hint=(None, None), size=(200, 50))
-        password_text = TextInput(multiline=False, size_hint=(None, None), width=200, height=30)
+        self.password_text = TextInput(multiline=False, size_hint=(None, None), width=200, height=30)
 
         back_button = Button(text='Back', size_hint=(None, None), size=(200, 50))
         login_button = Button(text='Login', size_hint=(None, None), size=(200, 50))
@@ -48,10 +68,11 @@ class LoginScreen(Screen):
                            pos_hint={'center_x': 0.5, 'center_y': 0.5})
 
         layout.add_widget(info_label)
+        layout.add_widget(info2_label)
         layout.add_widget(username)
-        layout.add_widget(username_text)
+        layout.add_widget(self.username_text)
         layout.add_widget(password)
-        layout.add_widget(password_text)
+        layout.add_widget(self.password_text)
         layout.add_widget(login_button)
         layout.add_widget(back_button)
 
@@ -63,7 +84,22 @@ class LoginScreen(Screen):
         self.manager.current = 'main'
 
     def manager_access(self, instance):
-        self.manager.current = 'manager'
+        login_credentials = {
+            'email': self.username_text.text,
+            'password': sha256_hash(self.password_text.text)
+        }
+        url = 'http://127.0.0.1:5000/log-in'
+        response = requests.get(url, json=login_credentials)
+        if response.status_code == 200:
+            self.manager.current = 'manager'
+        elif response.status_code == 400:
+            print(f"No such user {self.username_text.text}")
+        elif response.status_code ==300:
+            print("Wrong password")
+
+
+
+
 
 
 class RegisterScreen(Screen):
@@ -72,12 +108,11 @@ class RegisterScreen(Screen):
         self.orientation = 'vertical'
         info_label = Label(text='', size_hint=(None, None), size=(200, 50))
         username = Label(text='E-mail:', size_hint=(None, None), size=(200, 50))
-        username_text = TextInput(multiline=False, size_hint=(None, None), width=200, height=30)
+        self.username_text = TextInput(multiline=False, size_hint=(None, None), width=200, height=30)
         password = Label(text='Password:', size_hint=(None, None), size=(200, 50))
-        password_text = TextInput(multiline=False, size_hint=(None, None), width=200, height=30)
+        self.password_text = TextInput(multiline=False, size_hint=(None, None), width=200, height=30)
         password2 = Label(text='Repeat Password:', size_hint=(None, None), size=(200, 50))
-        password_text2 = TextInput(multiline=False, size_hint=(None, None), width=200, height=30)
-
+        self.password_text2 = TextInput(multiline=False, size_hint=(None, None), width=200, height=30)
         back_button = Button(text='Back', size_hint=(None, None), size=(200, 50))
         register_button = Button(text='Register', size_hint=(None, None), size=(200, 50))
         layout = BoxLayout(orientation='vertical', spacing=10, size_hint=(None, None), size=(200, 200),
@@ -85,11 +120,11 @@ class RegisterScreen(Screen):
 
         layout.add_widget(info_label)
         layout.add_widget(username)
-        layout.add_widget(username_text)
+        layout.add_widget(self.username_text)
         layout.add_widget(password)
-        layout.add_widget(password_text)
+        layout.add_widget(self.password_text)
         layout.add_widget(password2)
-        layout.add_widget(password_text2)
+        layout.add_widget(self.password_text2)
         layout.add_widget(register_button)
         layout.add_widget(back_button)
 
@@ -102,9 +137,3 @@ class RegisterScreen(Screen):
 
     def switch_to_main_screen(self, instance):
         self.manager.current = 'main'
-
-
-
-
-
-
--- app.py
+++ app.py
@@ -43,6 +43,25 @@ def add_car():
     conn.close()
     return f'Car {brand_name} added successfully!'
 
+@app.route('/log-in', methods=['GET'])
+def log_in():
+    conn = connect_to_db()
+    cur = conn.cursor()
+    data = request.get_json()
+
+    cur.execute(f"""SELECT password FROM customer WHERE mail = '{data['email']}'""")
+    try:
+        db_password = cur.fetchone()[0]
+        print(db_password, data['password'])
+        if db_password == data['password']:
+            return jsonify({'success': True}), 200
+        else:
+            return jsonify({'Wrong password': True}), 300
+    except:
+        return jsonify({'error': 'Wrong username or password'}), 400
+    finally:
+        cur.close()
+        conn.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
